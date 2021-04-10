ac611347be8dfd7fa1a29de1b862d36b8b6df8d0
Abidán Brito Clavijo <abidan.brito@gmail.com>
2021-04-10 23:22:20 +0100

    feat: implemented respawning of the player

---
+++ Assets/GamePlay/Scripts/Player/Respawn.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : BaseGame
+{
+    GameObject respawnPosition, respawnTrigger;
+    BoxCollider respawnTriggerCollider;
+
+    void Awake()
+    {
+        respawnPosition = GameObject.FindGameObjectWithTag(GameConstants.RESPAWN_POSITION_TAG);
+        respawnTrigger = GameObject.FindGameObjectWithTag(GameConstants.RESPAWN_TRIGGER_TAG);
+        respawnTriggerCollider = respawnTrigger.GetComponentInParent<BoxCollider>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Check for the player as the collider
+        if (other.CompareTag(GameConstants.PLAYER_TAG) 
+        && this.transform.position.y <= other.transform.position.y) 
+        {
+            other.transform.position = respawnPosition.transform.position;
+        } 
+    }
+}
