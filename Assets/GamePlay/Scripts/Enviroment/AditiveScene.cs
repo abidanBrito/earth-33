04dec0ddeb6b286d66496cc50a1a5eaed890e161
Yeray Candel <ycansam@epsg.upv.es>
2021-06-15 17:04:12 +0200

    Añadida escena aditiva boss

---
+++ Assets/GamePlay/Scripts/Enviroment/AditiveScene.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AditiveScene : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == player.tag){
+            SceneManager.LoadScene("BossScene", LoadSceneMode.Additive);
+        }
+    }
+    private void OnTriggerExit(Collider other){
+        if(other.tag == player.tag){
+            SceneManager.UnloadSceneAsync("BossScene");
+        }
+    }
+        
+
+}
