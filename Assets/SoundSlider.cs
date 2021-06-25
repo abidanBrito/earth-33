3aa0dcf8cd8f476d865328e54f3e80b7865117ef
Losina <losina.dd@gmail.com>
2021-06-25 11:37:08 +0200

    Algunos bugs arreglados, y terminada la interfaz

--- Assets/SoundSlider.cs
+++ Assets/SoundSlider.cs
@@ -13,21 +13,13 @@ public class SoundSlider : MonoBehaviour
 
     public void SetLevelMusica(float SliderValue)
     {
-        mixer.SetFloat("Musica", Mathf.Log10(SliderValue) * 20);
+        mixer.SetFloat("MusicaV", Mathf.Log10(SliderValue) * 20);
         mixer.SetFloat("MusicaVol", Mathf.Log10(SliderValue) * 20);
 
     }
 
     public void SetLevelEfectos(float SliderValue)
     {
-        mixer.SetFloat("EfectosVolTornillos", Mathf.Log10(SliderValue) * 20);
-        mixer.SetFloat("EfectosVolSpheres", Mathf.Log10(SliderValue) * 20);
-        mixer.SetFloat("EfectosVolProjectile", Mathf.Log10(SliderValue) * 20);
-        mixer.SetFloat("EfectosVolShot", Mathf.Log10(SliderValue) * 20);
-        mixer.SetFloat("EfectosVolEnemies", Mathf.Log10(SliderValue) * 20);
-        mixer.SetFloat("EfectosVolHealer", Mathf.Log10(SliderValue) * 20);
-        mixer.SetFloat("EfectosVolOtros", Mathf.Log10(SliderValue) * 20);
         mixer.SetFloat("EfectosVolSonidos", Mathf.Log10(SliderValue) * 20);
-        mixer.SetFloat("EfectosVolAbilidades", Mathf.Log10(SliderValue) * 20);
     }
 }
